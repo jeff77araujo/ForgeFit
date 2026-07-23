1c14fc6e81819d3a16ec4cb3095e82aadbf568a2
Jefferson <jefferson_lord@yahoo.com.br>
2026-07-22 21:41:58 -0300

    Initial Commit

---
+++ ForgeFit/ForgeFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForgeFitApp.swift
+//  ForgeFit
+//
+//  Created by Jeff Araujo on 22/07/26.
+//
+
+import SwiftUI
+
+@main
+struct ForgeFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ForgeFitTests/ForgeFitTests.swift
@@ -0,0 +1,18 @@
+//
+//  ForgeFitTests.swift
+//  ForgeFitTests
+//
+//  Created by Jeff Araujo on 22/07/26.
+//
+
+import Testing
+
+struct ForgeFitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
